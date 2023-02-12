e8ca69c7d51e64047479d20c2d3d0299169ed597
MOHAMEDmakhloufi <medmak.943@gmail.com>
2023-02-12 06:48:00 +0100

    version0.1

---
+++ src/main/java/com/fsb/digital_banking_backend/DigitalBankingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fsb.digital_banking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBankingBackendApplication.class, args);
+    }
+
+}
